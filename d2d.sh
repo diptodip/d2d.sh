38810aeb5d685a821d61ab044925b8e8c8a8ca33
Diptodip Deb <d2d.tar.gz@gmail.com>
2018-11-26 16:43:33 -0500

    Add commands for CSS minification

--- d2d.sh
+++ d2d.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# minify css
+
+echo -e "\033[0;32m[INFO] Minifying CSS...\033[0m"
+
+FLOW=layouts/partials/flow.html
+MINFLOW=layouts/partials/flow.min.html
+
+NIGHT=layouts/partials/night.html
+MINNIGHT=layouts/partials/night.min.html
+
+
+cat $FLOW | sed -e 's/  //g; s/  //g; s/\([:{;,]\) /\1/g; s/ {/{/g; s/\/\*.*\*\///g; /^$/d;' | sed -e :a -e '$!N; s/\n\(.\)/\1/; ta' > $MINFLOW
+cat $NIGHT | sed -e 's/  //g; s/  //g; s/\([:{;,]\) /\1/g; s/ {/{/g; s/\/\*.*\*\///g; /^$/d;' | sed -e :a -e '$!N; s/\n\(.\)/\1/; ta' > $MINNIGHT
+
 echo -e "\033[0;32m[INFO] Deploying updates to GitHub...\033[0m"
 
 # Build the project.
@@ -22,3 +36,6 @@ git push origin master
 
 # Come Back up to the Project Root
 cd ..
+
+# success
+echo -e "\033[0;32m[INFO] OK!\033[0m"
